62149a24f6ebfcc742494ea5a5bdc19ac0809671
Sujeet Singh Kushwah <sujeetsinghkushwah72@gmail.com>
2026-02-05 01:06:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:"http://localhost:5173",
+    origin:"https://food-delivery-mern-1sq8.onrender.com",
     credentials:true,
     methods:['POST','GET']
 }
@@ -31,7 +31,7 @@ app.set("io",io) // isse io kahi bhi use kar sakte hai
 
 const port=process.env.PORT || 5000
 app.use(cors({ //kon se url backend access kr sakta hai
-    origin:"http://localhost:5173",
+    origin:"https://food-delivery-mern-1sq8.onrender.com",
     credentials:true
 }))
 app.use(express.json()) // converting data into json
